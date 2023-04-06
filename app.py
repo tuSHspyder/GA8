434f6e741c226ec873a120ea7eccdad1e762fbb5
tuSHspyder <130055030+tuSHspyder@users.noreply.github.com>
2023-04-06 17:48:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,5 @@ with st.echo(code_location='below'):
   number3 = st.number_input('Insert a number 3')
 
   max_val = max(number1, number2, number3)
-  
-  st.text('The largest among the 3 given numbers is:')
-  st.write(max_val)
+
+  st.write('The largest among the 3 given numbers is: ',max_val)
